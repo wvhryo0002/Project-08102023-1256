eb60a64f6b1e0f2cd9b28511e42b55e2db837966
wvhryo0002 <147188287+wvhryo0002@users.noreply.github.com>
2023-10-08 14:40:26 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,37 +2,16 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 setlocal enabledelayedexpansion
 
-:: Define variables
-set "blender_version=3.6.4"
-set "blender_url=https://download.blender.org/release/Blender%blender_version%/blender-%blender_version%-windows-x64.zip"
-set "blender_dir=C:\Program Files\Blender Foundation\Blender %blender_version%"
-set "python_path=C:\Python27\Scripts"
-
-:: Step 1: Download Blender
-:: echo Downloading Blender %blender_version%...
-:: curl -o "blender.msi" "https://www.blender.org/download/release/Blender3.6/blender-3.6.4-windows-x64.msi/"
-
-:: Step 2: Install Blender
-:: echo Installing Blender %blender_version%...
-:: msiexec /i "%output%" /qn /norestart
-
-:: Step 3: Install gdown
-echo Installing gdown using pip...
-pip install gdown
+:: Download Blender
+curl -o "blender.msi" "https://mirrors.ocf.berkeley.edu/blender/release/Blender3.6/blender-3.6.4-windows-x64.msi"
+msiexec /i blender.msi /qn /norestart
 
 :: Define the Google Drive file URL and destination folder
 set "google_drive_url=https://drive.google.com/uc?id=1annnYF8aM2lB1-jNJourh86vSY2-MGXP"
 set "destination_folder=C:\Program Files\Blender Foundation\Blender 3.6"
-
-:: Step 1: Download the file from Google Drive using gdown
-echo Downloading file from Google Drive...
+pip install gdown
 gdown "%google_drive_url%"
-
-:: Step 2: Unzip the downloaded file using 7-Zip
-echo Unzipping the downloaded file...
 "C:\Program Files\7-Zip\7z.exe" x "opengl32.zip" -o"%destination_folder%"
 
 :: Clean up: Remove the downloaded ZIP file
 del "opengl32.zip"
-
-echo File downloaded and unzipped successfully.
